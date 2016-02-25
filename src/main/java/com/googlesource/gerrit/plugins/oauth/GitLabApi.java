17092d52ca90959a280e209677d7f151a149bb46
ZhiFeng Hu <hufeng1987@gmail.com>
2016-02-25 13:14:26 +0800

    Update redirect_url
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- src/main/java/com/googlesource/gerrit/plugins/oauth/GitLabApi.java
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/GitLabApi.java
@@ -90,8 +90,7 @@ public class GitLabApi extends DefaultApi20 {
             request.addHeader("Authorization", prepareAuthorizationHeaderValue());
             request.addBodyParameter(GRANT_TYPE, GRANT_TYPE_VALUE);
             request.addBodyParameter(CODE, verifier.getValue());
-            request.addBodyParameter(OAuthConstants.REDIRECT_URI,
-                    config.getCallback());
+            request.addBodyParameter(REDIRECT_URI, config.getCallback());
             Response response = request.send();
             if (response.getCode() == SC_OK) {
                 Token t = api.getAccessTokenExtractor().extract(response.getBody());
