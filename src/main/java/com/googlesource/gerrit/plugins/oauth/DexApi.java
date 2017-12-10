c8cb1fa6728929f481cd2712b409c91c7457c18a
Piotr Wolny <gildur@gmail.com>
2017-12-10 23:02:11 +0100

    Refactoring of duplicated code
    Change-Id: Id47d30495f5983679f8aef0fe4c5039a1d81365e

--- src/main/java/com/googlesource/gerrit/plugins/oauth/DexApi.java
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/DexApi.java
@@ -18,12 +18,7 @@ import org.scribe.builder.api.DefaultApi20;
 import org.scribe.extractors.AccessTokenExtractor;
 import org.scribe.extractors.JsonTokenExtractor;
 import org.scribe.model.OAuthConfig;
-import org.scribe.model.OAuthConstants;
-import org.scribe.model.OAuthRequest;
-import org.scribe.model.Response;
-import org.scribe.model.Token;
 import org.scribe.model.Verb;
-import org.scribe.model.Verifier;
 import org.scribe.oauth.OAuthService;
 import org.scribe.utils.OAuthEncoder;
 
@@ -60,77 +55,11 @@ public class DexApi extends DefaultApi20 {
 
   @Override
   public OAuthService createService(OAuthConfig config) {
-    // TODO can't use this until updating to newer scribe lib
-    // return new OAuth20ServiceImpl(this,config);
-    return new DexOAuthService(this, config);
+    return new OAuth20ServiceImpl(this, config);
   }
 
   @Override
   public AccessTokenExtractor getAccessTokenExtractor() {
     return new JsonTokenExtractor();
-  }
-
-  private static final class DexOAuthService implements OAuthService {
-
-    private static final String VERSION = "2.0";
-
-    private static final String GRANT_TYPE = "grant_type";
-    private static final String GRANT_TYPE_VALUE = "authorization_code";
-
-    private final DefaultApi20 api;
-    private final OAuthConfig config;
-
-    /**
-     * Default constructor
-     *
-     * @param api OAuth2.0 api information
-     * @param config OAuth 2.0 configuration param object
-     */
-    public DexOAuthService(DefaultApi20 api, OAuthConfig config) {
-      this.api = api;
-      this.config = config;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public Token getAccessToken(Token requestToken, Verifier verifier) {
-      OAuthRequest request =
-          new OAuthRequest(api.getAccessTokenVerb(), api.getAccessTokenEndpoint());
-      request.addBodyParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
-      request.addBodyParameter(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
-      request.addBodyParameter(OAuthConstants.CODE, verifier.getValue());
-      request.addBodyParameter(OAuthConstants.REDIRECT_URI, config.getCallback());
-      if (config.hasScope()) {
-        request.addBodyParameter(OAuthConstants.SCOPE, config.getScope());
-      }
-      request.addBodyParameter(GRANT_TYPE, GRANT_TYPE_VALUE);
-      Response response = request.send();
-      return api.getAccessTokenExtractor().extract(response.getBody());
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public Token getRequestToken() {
-      throw new UnsupportedOperationException(
-          "Unsupported operation, please use 'getAuthorizationUrl' and redirect your users there");
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public String getVersion() {
-      return VERSION;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void signRequest(Token accessToken, OAuthRequest request) {
-      request.addQuerystringParameter(OAuthConstants.ACCESS_TOKEN, accessToken.getToken());
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public String getAuthorizationUrl(Token requestToken) {
-      return api.getAuthorizationUrl(config);
-    }
   }
 }
