4218f62eba4caf55ec764d29c7bed3f56d620b96
David Ostrovsky <david@ostrovsky.org>
2020-03-08 18:14:04 +0100

    Add unit tests for oauth api providers
    This is a fix to regression from: I68717ebb988. The following oauth
    providers are fixed:
    
    * AirVantage
    * Bitbucket
    * Dex
    * Keycloak
    
    Bug: Issue https://github.com/davido/gerrit-oauth-provider/issues/139
    Change-Id: If23af40894b363dbc1a15e7cc98a3688d6f98ba2

--- src/main/java/com/googlesource/gerrit/plugins/oauth/KeycloakApi.java
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/KeycloakApi.java
@@ -15,9 +15,6 @@
 package com.googlesource.gerrit.plugins.oauth;
 
 import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.extractors.OAuth2AccessTokenExtractor;
-import com.github.scribejava.core.extractors.TokenExtractor;
-import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.oauth2.bearersignature.BearerSignature;
 import com.github.scribejava.core.oauth2.bearersignature.BearerSignatureURIQueryParameter;
 import com.github.scribejava.core.oauth2.clientauthentication.ClientAuthentication;
@@ -43,11 +40,6 @@ public class KeycloakApi extends DefaultApi20 {
   @Override
   public String getAccessTokenEndpoint() {
     return String.format("%s/auth/realms/%s/protocol/openid-connect/token", rootUrl, realm);
-  }
-
-  @Override
-  public TokenExtractor<OAuth2AccessToken> getAccessTokenExtractor() {
-    return OAuth2AccessTokenExtractor.instance();
   }
 
   @Override
